a040d12c854756b4f2ddf2001544ed952dc693bf
dimkanevstruev <nevstruev.dmitriy@mail.ru>
2019-03-26 20:57:17 +0300

    esay change

--- graphcreator.cpp
+++ graphcreator.cpp
@@ -282,13 +282,7 @@ void GraphCreator::PullLegsTogether() {
 void GraphCreator::RunWay() {
   std::vector <const Edge*> way;
   std::vector <std::string> path;
-
-
-  std::vector<float> st;
-  path.push_back("HeadYaw");
-  st.push_back(0);
-  graph_.SetStiffness(path, st, 0.1);
-
+  
   SmallLog("ENTER vertexes names", 2);
 
   for (int i = 0; i < 2; ++i) {
